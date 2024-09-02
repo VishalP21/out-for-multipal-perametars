a8491d9de0c8e881065415ac06942d89a32f804a
Vishal Prajapati <vishalp951295@gmail.com>
2024-09-02 10:02:35 +0530

    Add project files.

---
+++ out for multipal perametars/Program.cs
@@ -0,0 +1,8 @@
+﻿using out_for_multipal_perametars;
+
+employee employee = new();
+
+int x = employee.sum(8, 5, out int c);
+
+Console.WriteLine(c);
+Console.WriteLine(x);
---
+++ out for multipal perametars/employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace out_for_multipal_perametars
+{
+    public class employee
+    {
+        public int sum(int a, int b, out int c)
+        {
+            c = a + b;
+            return a - b;
+        }
+    }
+}
